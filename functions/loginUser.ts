a6d1a185f29249f3dfad64a67b02601ad3640ad7
bagalumaji <bagalumaji@gmail.com>
2023-12-08 08:46:05 -0800

    login user function

---
+++ functions/loginUser.ts
@@ -0,0 +1,5 @@
+const login = (name:string,password:string,isStudent:boolean)=>{}
+login("sayaji","sayaji",true);
+
+const sum = (num1:number,num2:number):number=> num1 + num2;
+console.log(sum(11, 22));
