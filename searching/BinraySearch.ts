43e7dcfa20636f038e4e2686fc528c2c3bf33db3
umajibagal <bagalumaji@gmail.com>
2024-08-19 19:14:55 +0530

    converted to function

--- searching/BinraySearch.ts
+++ searching/BinraySearch.ts
@@ -1,29 +1,31 @@
-let values=[1,2,3,4,5,6,7,8,9,10];
-let begin=0;
-let end = values.length-1;
-let mid:number;
-let val=4;
-let f=false;
-while(begin<=end){
-    mid = Math.floor((begin+end)/2);
-    console.log("mid : "+mid);
-    console.log("begin : "+begin);
-    console.log("eng : "+end);
-    console.log("value : "+values[mid])
-    if(val == values[mid]){
-        f=true;
-        break;
-    }
 
-    if(val<values[mid]){
-        end=mid-1;
-    }else{
-        begin=mid+1;
-    }
+function searchByUsingBinarySearch(val:number,values:number[]):boolean {
+    let begin:number = 0;
+    let end: number = values.length - 1;
+    let mid: number;
+    let result:boolean = false;
+    while (begin <= end) {
+        mid = Math.floor((begin + end) / 2);
+        // console.log("mid : " + mid);
+        // console.log("begin : " + begin);
+        // console.log("eng : " + end);
+        // console.log("value : " + values[mid])
+        if (val == values[mid]) {
+            result= true;
+            break;
+        }
 
+        if (val < values[mid]) {
+            end = mid - 1;
+        } else {
+            begin = mid + 1;
+        }
+
+    }
+    return result;
 }
-if(f){
-    console.log("found the value");
-}else{
-    console.log("not found the value");
-}
+
+let values:number[]=[1,2,3,4,5,6,7,8,9,10];
+const val:number=4;
+const result = searchByUsingBinarySearch(val,values)?`${val} found`:` ${val} not found`;
+console.log(result);
