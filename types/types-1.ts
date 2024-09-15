01cca3b04b836192bf6af1c09cb3642dd26d4f1f
Umaji Bagal <bagalumaji@gmail.com>
2024-09-15 10:46:36 +0530

    types in typescript

---
+++ types/types-1.ts
@@ -0,0 +1,34 @@
+//pre
+//ref
+
+//number boolean String
+
+//object classes function array
+
+let a=12;
+let b:number=22;
+let arr:[];
+let firstname:string;
+let num:[number,string]=[1,"hi"];
+let temp:any;
+temp=1;
+temp=false;
+
+let f:unknown;
+f=11;
+
+//never
+
+// function test():never{
+//     while (true){
+//         console.log("hello world");
+//     }
+// }
+//
+// test();
+// console.log("hello world");
+
+function demo():void{
+    console.log("hello world");
+}
+
