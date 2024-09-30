0df0459976c5570e37c4bafea6b9c59955ce75a8
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-09-30 03:08:07 +0000

    reduce

--- arrays/array-2.ts
+++ arrays/array-2.ts
@@ -2,3 +2,8 @@ const arr=['abc','hi','hello','js'];
 const findIndex = arr.findIndex(a => a.length>=2);
 console.log(findIndex);
 console.log(arr.find(s => s.length >= 2));
+
+const values=[1,2,3,4,5];
+
+const sum = values.reduce((a,s)=>a+s,0);
+console.log(sum);
