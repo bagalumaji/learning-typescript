5e51a54c396c9a6e666369fc771fcfe9ecf6a964
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-09-30 02:53:19 +0000

    array methods

---
+++ arrays/arrays.ts
@@ -0,0 +1,21 @@
+const nums=[11,22,33,44,55,66,77]
+console.log(`array elements : ${nums}`)
+
+// console.log(nums.push(99)); // add element and return the lenght of array
+
+// console.log(nums.pop()); // removes and returun last element
+
+// console.log(nums.unshift(88)); // add element and return the lenght of array
+// console.log(nums.shift()) // // removes and returun first element
+// const test=[1,2,3,4];
+// const newArr = nums.concat([1,2,3,4])
+// console.log(newArr);
+
+// const str =  test.join();
+
+// console.log(str)
+
+console.log(nums.indexOf(33));
+
+
+
