9deb750576c95866890b9b7d070354ac50852f42
umaji bagal <bagalumaji@gmail.com>
2024-11-17 21:19:14 +0530

    string methods

---
+++ strings/string-1.ts
@@ -0,0 +1,13 @@
+const str="hello world";
+//print len of string
+console.log(str.length);
+
+//string to upper case
+console.log(str.toUpperCase());
+
+//string to lower case
+console.log(str.toLowerCase())
+
+// includes to check sub string is present or not
+// @ts-ignore
+console.log(str.includes("hello"));
