fc3d1bea1f23a59a11948a39970f764c78c03143
umaji bagal <bagalumaji@gmail.com>
2025-01-22 10:28:01 +0530

    reverse string

--- strings/reverse-string.ts
+++ strings/reverse-string.ts
@@ -1,1 +1,8 @@
-const str="hello typescript";
+const str = "hello typescript";
+console.log(str.split("").reverse().join(''));
+
+let reverseString: string = '';
+for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+}
+console.log(reverseString);
